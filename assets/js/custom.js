769f3e4d80dae28de16955cc715a8955acaa6889
Harman Waseer <2403hwaseer@gmail.com>
2020-10-11 23:59:37 +0530

    carousel-added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -20,23 +20,23 @@
 
 
 	// Menu elevator animation
-	$('a[href*=\\#]:not([href=\\#])').on('click', function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				var width = $(window).width();
-				if(width < 991) {
-					$('.menu-trigger').removeClass('active');
-					$('.header-area .nav').slideUp(200);	
-				}				
-				$('html,body').animate({
-					scrollTop: (target.offset().top) - 130
-				}, 700);
-				return false;
-			}
-		}
-	});
+	// $('a[href*=\\#]:not([href=\\#])').on('click', function() {
+	// 	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	// 		var target = $(this.hash);
+	// 		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	// 		if (target.length) {
+	// 			var width = $(window).width();
+	// 			if(width < 991) {
+	// 				$('.menu-trigger').removeClass('active');
+	// 				$('.header-area .nav').slideUp(200);	
+	// 			}				
+	// 			$('html,body').animate({
+	// 				scrollTop: (target.offset().top) - 130
+	// 			}, 700);
+	// 			return false;
+	// 		}
+	// 	}
+	// });
 
 	$(document).ready(function () {
 	    $(document).on("scroll", onScroll);
