28642430e335097aa710cc5837fe5a80f2934edf
Harman Waseer <2403hwaseer@gmail.com>
2020-10-21 22:39:45 +0530

    navbar works

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -20,48 +20,48 @@
 
 
 	// Menu elevator animation
-	// $('a[href*=\\#]:not([href=\\#])').on('click', function() {
-	// 	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-	// 		var target = $(this.hash);
-	// 		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-	// 		if (target.length) {
-	// 			var width = $(window).width();
-	// 			if(width < 991) {
-	// 				$('.menu-trigger').removeClass('active');
-	// 				$('.header-area .nav').slideUp(200);	
-	// 			}				
-	// 			$('html,body').animate({
-	// 				scrollTop: (target.offset().top) - 130
-	// 			}, 700);
-	// 			return false;
-	// 		}
-	// 	}
-	// });
+	$('a[href*=\\#]:not([href=\\#])').on('click', function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				var width = $(window).width();
+				if(width < 991) {
+					$('.menu-trigger').removeClass('active');
+					$('.header-area .nav').slideUp(200);	
+				}				
+				$('html,body').animate({
+					scrollTop: (target.offset().top) - 130
+				}, 700);
+				return false;
+			}
+		}
+	});
 
-	// $(document).ready(function () {
-	//     $(document).on("scroll", onScroll);
+	$(document).ready(function () {
+	    $(document).on("scroll", onScroll);
 	    
-	//     //smoothscroll
-	//     $('a[href^="#"]').on('click', function (e) {
-	//         e.preventDefault();
-	//         $(document).off("scroll");
+	    //smoothscroll
+	    $('a[href^="#"]').on('click', function (e) {
+	        e.preventDefault();
+	        $(document).off("scroll");
 	        
-	//         $('a').each(function () {
-	//             $(this).removeClass('active');
-	//         })
-	//         $(this).addClass('active');
+	        $('a').each(function () {
+	            $(this).removeClass('active');
+	        })
+	        $(this).addClass('active');
 	      
-	//         var target = this.hash,
-	//         menu = target;
-	//        	var target = $(this.hash);
-	//         $('html, body').stop().animate({
-	//             scrollTop: (target.offset().top) - 130
-	//         }, 500, 'swing', function () {
-	//             window.location.hash = target;
-	//             $(document).on("scroll", onScroll);
-	//         });
-	//     });
-	// });
+	        var target = this.hash,
+	        menu = target;
+	       	var target = $(this.hash);
+	        $('html, body').stop().animate({
+	            scrollTop: (target.offset().top) - 130
+	        }, 500, 'swing', function () {
+	            window.location.hash = target;
+	            $(document).on("scroll", onScroll);
+	        });
+	    });
+	});
 
 	function onScroll(event){
 	    var scrollPos = $(document).scrollTop();
