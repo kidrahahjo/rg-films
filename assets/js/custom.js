81187a02ce04934622f7acfb1e324050d897ea25
Hardik Ojha <44747868+kidrahahjo@users.noreply.github.com>
2020-12-07 19:56:49 +0530

    Improve styling (#9)
    * add-media-section
    
    * add-carousel
    
    * Remove extra gallery section
    
    * carousel-added
    
    * gallery issue solved
    
    * style navbar
    
    * style news view
    
    * navbar works
    
    * required changes done
    
    * abc
    
    * popup not added
    
    * carousel changes done
    
    * final changes done
    
    * centered modal
    
    * scroll
    
    * scroll
    
    * scroll2
    
    * mobile view
    
    * image edit
    
    Co-authored-by: Harman Waseer <2403hwaseer@gmail.com>

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -103,17 +103,17 @@
 
 	function onScroll(event){
 	    var scrollPos = $(document).scrollTop();
-	    $('.nav a').each(function () {
-	        var currLink = $(this);
-	        var refElement = $(currLink.attr("href"));
-	        // if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height()  > scrollPos) {
-	        //     $('.nav ul li a').removeClass("active");
-	        //     currLink.addClass("active");
-	        // }
-	        // else{
-	        //     currLink.removeClass("active");
-	        // }
-	    });
+	    // $('.nav a').each(function () {
+	    //     var currLink = $(this);
+	    //     var refElement = $(currLink.attr("href"));
+	    //     if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height()  > scrollPos) {
+	    //         $('.nav ul li a').removeClass("active");
+	    //         currLink.addClass("active");
+	    //     }
+	    //     else{
+	    //         currLink.removeClass("active");
+	    //     }
+	    // });
 	}
 
 
